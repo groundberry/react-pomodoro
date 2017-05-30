f6222cfa80a5597d146dd88a361b85296ed4f02c
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2017-05-29 19:30:44 -0700

    Add enzyme to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Pomodoro clock</h2>
-        </div>
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <h2>Pomodoro clock</h2>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+describe('<App />', () => {
+  it('renders a title', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.text()).toBe('Pomodoro clock');
+  });
 });
